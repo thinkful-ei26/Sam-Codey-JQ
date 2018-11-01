fcc6176f24fe00442f974838b547427a7883d4b0
Sam Smiley <samtsmiley@gmail.com>
2018-11-01 08:38:21 -0700

    first commit

---
+++ drills.js
@@ -0,0 +1,10 @@
+'use strict';
+function main() {
+  // eslint-disable-next-line no-undef
+  $('thumbnail').on('click', function (event) {
+    console.log(event.target);
+  });
+}
+
+// eslint-disable-next-line no-undef
+$(main);
